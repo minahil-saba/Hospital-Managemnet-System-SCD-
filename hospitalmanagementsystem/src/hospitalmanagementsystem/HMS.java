fa7aec3018d83d73652667e91e51889266fc0389
minahil-saba <sabaminahil8@gmail.com>
2024-10-22 02:08:45 +0500

    Add files via upload

---
+++ hospitalmanagementsystem/src/hospitalmanagementsystem/HMS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalmanagementsystem;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class HMS {
+   public static void main(String[] args) {
+        Login ls=new Login();
+        ls.setVisible(true);
+        ls.pack();
+        ls.setLocationRelativeTo(null);
+    }
+}
